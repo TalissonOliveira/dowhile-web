313054efd198799d54df012de0358b3a7c15e195
Talisson Oliveira <talissonlux@outlook.com>
2021-10-22 17:52:49 -0300

    feat: shows messages in real time with socket.io

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import { api } from '../../services/api'
 
+import io from 'socket.io-client'
+
 import styles from './styles.module.scss'
 
 import logoImg from '../../assets/logo.svg'
@@ -14,8 +16,31 @@ interface Message {
     }
 }
 
+const messagesQueue: Message[] = []
+
+const socket = io('http://localhost:4000')
+
+socket.on('new_message', (newMessage: Message) => {
+    messagesQueue.push(newMessage)
+})
+
 export function MessageList() {
     const [messages, setMessages] = useState<Message[]>([])
+
+    useEffect(() => {
+        setInterval(() => {
+            if (messagesQueue.length > 0) {
+                setMessages(prevState => [
+                    messagesQueue[0],
+                    prevState[0],
+                    prevState[1]
+                ].filter(Boolean))
+
+                messagesQueue.shift() // remover primeiro item da fila
+            }
+        }, 3000)
+    }, []) 
+
 
     useEffect(() => {
         api.get<Message[]>('messages/last3').then(response => {
